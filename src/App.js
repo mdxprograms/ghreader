9b6187c9b02c87842080eed57206cb3cf53c75a1
Josh Waller <mdx.programs@gmail.com>
2020-06-11 13:16:28 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState, Children } from 'react';
+import { Octokit } from "@octokit/rest"
 import './App.css';
 
+const octokit = new Octokit({
+  auth: process.env.REACT_APP_GH_TOKEN
+});
+
+/**
+ * get users following -> octokit.users.listFollowedByAuthenticated();
+ * get users followers -> octokit.users.listFollowersForAuthenticatedUser();
+ * get issues assigned to user -> octokit.issues.list();
+ * get gists owned by user -> octokit.gists.list();
+ * get gist owned by user -> 
+ * octokit.gists.get({
+ *   gist_id,
+ * });
+ * get notifications for user -> octokit.activity.listNotificationsForAuthenticatedUser();
+ */
+
+const Loader = () =>
+  <div>Loading...</div>
+
 function App() {
+  const [user, setUser] = useState(null)
+  const [loaded, setLoaded] = useState(false)
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const { data } = await octokit.users.getAuthenticated()
+      setUser(data)
+      setLoaded(true)
+    }
+    fetchUser()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Github Reader</h1>
+      {!loaded && <Loader />}
+      {loaded && 
+        <>
+          {console.log(user)}
+          <h3>{user.name}</h3>
+          <img src={user.avatar_url} alt="profile img"/>
+          <ul>
+            <li>Followers: {user.followers}</li>
+            <li>Following: {user.following}</li>
+            <li>Public Repos: {user.public_repos}</li>
+            <li>Private: {user.total_private_repos}</li>
+          </ul>
+        </>  
+      }
     </div>
   );
 }
