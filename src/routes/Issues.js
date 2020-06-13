08d3e479f37230fb6324545bf0ac08d1403eddba
Josh <mdx.programs@gmail.com>
2020-06-13 12:09:31 -0400

    adding issues table fix with redux store

--- src/routes/Issues.js
+++ src/routes/Issues.js
@@ -1,22 +1,52 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react"
 import { useSelector, useDispatch } from 'react-redux'
+import { Table, ColumnGroup } from 'antd'
 import { issuesSelect, loadingSelect, fetchIssues } from '../store/issuesSlice'
 import Loader from '../components/Loader'
 
-const Issues = () => {
+const IssueTable = ({ issues }) => { 
+  const issuesData = issues.map(issue => ({
+    key: issue.id,
+    title: issue.title,
+    ghLink: issue.html_url,
+    state: issue.state
+  }))
+  const issuesColumns = [
+    {
+      key: 'title',
+      dataIndex: 'title',
+      title: 'Title'
+    },
+    {
+      key: 'ghLink',
+      dataIndex: 'ghLink',
+      title: 'Github url'
+    },
+    {
+      key: 'state',
+      dataIndex: 'state',
+      title: 'Issue Status'
+    }
+  ]
+  return <Table dataSource={issues} columns={issuesColumns} />
+}
+
+function Issues () {
   const dispatch = useDispatch()
-  const issues = useSelector(issuesSelect)
-  const loading = useSelector(loadingSelect)
 
   useEffect(() => {
     dispatch(fetchIssues())
-  }, [dispatch])
+    // eslint-disable-next-line
+  }, [])
+
+  const issues = useSelector(issuesSelect)
+  const loading = useSelector(loadingSelect)
 
   return (
     <div>
-      {loading && <Loader loaded={!loading} />}
-      {!loading && issues && (
-        issues.map(issue => <p key={issue.id}>{issue.title}</p>)
+      {(loading && issues.length === 0) && <Loader loaded={!loading} />}
+      {(!loading && issues.length > 0) && (
+        <IssueTable issues={issues} />
       )}
     </div>
   )
